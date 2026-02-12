88d260220fef74eba458a7cf5d7f4a5335eba0f9
Lucagalli01 <lucaeg01@gmail.com>
2026-02-12 14:30:56 -0300

    Add files via upload

---
+++ assets/js/footer.js
@@ -0,0 +1,7 @@
+
+document.getElementById("footer").innerHTML = `
+<div class="footer-content">
+  <span>© La Fractura</span>
+  <span>Contacto · Archivo · Manifiesto</span>
+</div>
+`;
---
+++ assets/js/header.js
@@ -0,0 +1,21 @@
+
+document.getElementById("header").innerHTML = `
+<div class="header-content">
+  <div class="brand">
+    <span class="brand-symbol">✶</span>
+    <span class="site-title">LA FRACTURA</span>
+  </div>
+
+  <nav>
+    <ul>
+      <li><a href="/lafractura2/index.html">inicio</a></li>
+      <li><a href="/lafractura2/archivo.html">archivo</a></li>
+      <li><a href="/lafractura2/manifiesto.html">manifiesto</a></li>
+    </ul>
+  </nav>
+
+  <a class="subscribe-btn" href="https://forms.gle/" target="_blank">
+    suscribirse
+  </a>
+</div>
+`;
