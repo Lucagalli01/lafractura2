b007dad385a69abbd65c02e64daec473a73503cb
Lucagalli01 <lucaeg01@gmail.com>
2026-02-12 14:23:38 -0300

    prueba

---
+++ assets/js/footer.js
@@ -0,0 +1,5 @@
+
+document.getElementById("footer").innerHTML = `
+<div class="footer-content">
+  <small>© La Fractura</small>
+</div>`;
---
+++ assets/js/header.js
@@ -0,0 +1,12 @@
+
+document.getElementById("header").innerHTML = `
+<div class="header-content">
+  <div class="site-title">LA FRACTURA</div>
+  <nav>
+    <ul>
+      <li><a href="/lafractura2/index.html">inicio</a></li>
+      <li><a href="/lafractura2/archivo.html">archivo</a></li>
+      <li><a href="/lafractura2/manifiesto.html">manifiesto</a></li>
+    </ul>
+  </nav>
+</div>`;
